77683e124115f0ee3f35de270e4453ff0a4fa6ca
Komal Barge <48613817+komalbarge45@users.noreply.github.com>
2020-03-21 20:10:34 -0400

    model name changes

--- Assignment2/1095709_Assignment2.py
+++ Assignment2/1095709_Assignment2.py
@@ -209,19 +209,19 @@ from keras.models import model_from_json
 
 # serialize model to JSON
 model_json = model.to_json()
-with open("model.json", "w") as json_file:
+with open("1095709_1dconv_reg.json", "w") as json_file:
     json_file.write(model_json)
 # serialize weights to HDF5
-model.save_weights("model.h5")
+model.save_weights("1095709_1dconv_reg.h5")
 print("Saved model to disk")
 
 # load json and create model
-json_file = open('model.json', 'r')
+json_file = open('1095709_1dconv_reg.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 loaded_model = model_from_json(loaded_model_json)
 # load weights into new model
-loaded_model.load_weights("model.h5")
+loaded_model.load_weights("1095709_1dconv_reg.h5")
 print("Loaded model from disk")
  
 # evaluate loaded model on test data
